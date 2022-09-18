48409989b0e7573daad6ae1645a077aaa02d05a1
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-09-18 22:13:54 +0200

    Add alpha-version lap feature

--- app/src/main/java/pl/mateusz/stopwatch/MainActivity.java
+++ app/src/main/java/pl/mateusz/stopwatch/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Html;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView timeView;
     private ImageButton buttonStartStop;
     private ImageButton buttonReset;
+    private LinearLayout layoutLaps;
 
 
     @Override
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         timeView = findViewById(R.id.text_time);
+        layoutLaps = findViewById(R.id.layout_laps);
         // loading state if recreated
         if (savedInstanceState != null) {
             deciseconds = savedInstanceState.getInt("deciseconds");
@@ -39,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         buttonStartStop = findViewById(R.id.button_start_or_stop);
         buttonReset = findViewById(R.id.button_reset_or_lap);
         buttonStartStop.setOnClickListener(e -> onClickStartStop());
-        buttonReset.setOnClickListener(e -> onClickReset());
+        buttonReset.setOnClickListener(e -> onClickResetLap());
         // configuring timer runnable
         timerRunnable = new Runnable() {
             @Override
@@ -48,13 +53,7 @@ public class MainActivity extends AppCompatActivity {
                     deciseconds++;
                     handler.postDelayed(this, 100);
                 }
-                int decis = deciseconds % 10;
-                int seconds = (deciseconds % 600) / 10;
-                int minutes = (deciseconds % 36000) / 600;
-                int hours = (deciseconds / 36000);
-                String newTime = String.format(Locale.getDefault(), "%d:%02d:%02d.%d",
-                        hours, minutes, seconds, decis);
-                timeView.setText(newTime);
+                timeView.setText(formatToTime(deciseconds));
             }
         };
         // executing timer runnable to display initial time
@@ -83,19 +82,46 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void onClickReset() {
+    private void onClickResetLap() {
         if (!running) { // reset function
             deciseconds = 0;
             handler.post(timerRunnable);
             buttonReset.setImageResource(R.drawable.lap);
             buttonReset.setVisibility(View.GONE);
+            layoutLaps.removeAllViews();
+            lapCounter = 0;
+            previousLapTime = 0;
         } else { // lap function
-            LinearLayout layoutLaps = findViewById(R.id.layout_laps);
             TextView lapView = new TextView(this);
-            lapView.setGravity(Gravity.CENTER_HORIZONTAL);
+            //lapView.setGravity(Gravity.CENTER_HORIZONTAL);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.gravity = Gravity.CENTER_HORIZONTAL;
+            params.setMargins(10,10,10,10);
+            lapView.setLayoutParams(params);
             int difference = deciseconds - previousLapTime;
             previousLapTime = deciseconds;
+            lapCounter++;
+            String formatedDifference = "&#160&#160&#160&#160&#160<font color=#FF0000>" + "+"
+                    + formatToTime(difference)
+                    + "&#160&#160&#160&#160&#160</font";
+            String formatedCurrentTime = "<font color=#000000>" + formatToTime(deciseconds)
+                    + "</font";
+            String formatedLapCounter = String.format(Locale.getDefault(), "%02d",
+                    lapCounter);
+            String lap = formatedLapCounter + " " + formatedDifference + " " + formatedCurrentTime;
+            lapView.setText(Html.fromHtml(lap));
+            lapView.setTextSize(TypedValue.COMPLEX_UNIT_SP,20);
+            layoutLaps.addView(lapView);
         }
+    }
+
+    private String formatToTime(int decisecs) {
+        int decis = (deciseconds % 10);
+        int seconds = (deciseconds % 600) / 10;
+        int minutes = (deciseconds % 36000) / 600;
+        int hours = (deciseconds / 36000);
+        return String.format(Locale.getDefault(), "%d:%02d:%02d.%d",
+                hours, minutes, seconds, decis);
     }
 
 }
