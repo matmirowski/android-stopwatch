180eadab862502129a9649c2258ef99679dd2cd8
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-09-19 13:03:39 +0200

    Fix app crashing when rotated device

--- app/src/main/java/pl/mateusz/stopwatch/MainActivity.java
+++ app/src/main/java/pl/mateusz/stopwatch/MainActivity.java
@@ -2,8 +2,6 @@ package pl.mateusz.stopwatch;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.text.HtmlCompat;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Html;
@@ -12,10 +10,10 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,13 +22,13 @@ public class MainActivity extends AppCompatActivity {
     private int previousLapTime = 0;
     private boolean running = false;
     private final Handler handler = new Handler();
+    private ArrayList<String> lapList = new ArrayList<>();
     private Runnable timerRunnable;
     private TextView timeView;
     private ImageButton buttonStartStop;
-    private ImageButton buttonReset;
+    private ImageButton buttonResetLap;
     private ScrollView scrollView;
     private LinearLayout layoutLaps;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +37,14 @@ public class MainActivity extends AppCompatActivity {
         timeView = findViewById(R.id.text_time);
         layoutLaps = findViewById(R.id.layout_laps);
         scrollView = findViewById(R.id.scrollview_laps);
-        // loading state if recreated
-        if (savedInstanceState != null) {
-            deciseconds = savedInstanceState.getInt("deciseconds");
-            running = savedInstanceState.getBoolean("running");
-        }
-        // adding onClickListeners
         buttonStartStop = findViewById(R.id.button_start_or_stop);
-        buttonReset = findViewById(R.id.button_reset_or_lap);
+        buttonResetLap = findViewById(R.id.button_reset_or_lap);
+        // loading state if recreated
+        if (savedInstanceState != null)
+                this.loadSavedInstanceState(savedInstanceState);
+        // adding onClickListeners
         buttonStartStop.setOnClickListener(e -> onClickStartStop());
-        buttonReset.setOnClickListener(e -> onClickResetLap());
+        buttonResetLap.setOnClickListener(e -> onClickResetLap());
         // configuring timer runnable
         timerRunnable = new Runnable() {
             @Override
@@ -68,55 +64,60 @@ public class MainActivity extends AppCompatActivity {
     public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
         savedInstanceState.putInt("deciseconds", deciseconds);
         savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putInt("lapCounter", lapCounter);
+        savedInstanceState.putInt("previousLapTime", previousLapTime);
+        savedInstanceState.putStringArrayList("lapList", lapList);
         super.onSaveInstanceState(savedInstanceState);
     }
 
     private void onClickStartStop() {
         if (!running) {
             if (deciseconds == 0) // start
-                buttonReset.setVisibility(View.VISIBLE);
+                buttonResetLap.setVisibility(View.VISIBLE);
             running = true;
             handler.postDelayed(timerRunnable, 100);
-            buttonReset.setImageResource(R.drawable.lap);
+            buttonResetLap.setImageResource(R.drawable.lap);
             buttonStartStop.setImageResource(R.drawable.pause);
-        } else { // pause function
+        } else { // pause
             running = false;
             buttonStartStop.setImageResource(R.drawable.start);
-            buttonReset.setImageResource(R.drawable.reset);
+            buttonResetLap.setImageResource(R.drawable.reset);
         }
     }
 
     private void onClickResetLap() {
         if (!running) { // reset function
-            deciseconds = 0;
-            handler.post(timerRunnable);
-            buttonReset.setImageResource(R.drawable.lap);
-            buttonReset.setVisibility(View.GONE);
-            layoutLaps.removeAllViews();
-            lapCounter = 0;
-            previousLapTime = 0;
+            this.reset();
         } else { // lap function
-            TextView lapView = new TextView(this);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.gravity = Gravity.CENTER_HORIZONTAL;
-            params.setMargins(10,10,10,10);
-            lapView.setLayoutParams(params);
-            int difference = deciseconds - previousLapTime;
-            previousLapTime = deciseconds;
-            lapCounter++;
-            String formatedDifference = "&#160&#160&#160&#160&#160<font color=#FF0000>" + "+"
-                    + formatToTime(difference)
-                    + "&#160&#160&#160&#160&#160</font>";
-            String formatedCurrentTime = "<font color=#000000>" + formatToTime(deciseconds)
-                    + "</font>";
-            String formatedLapCounter = String.format(Locale.getDefault(), "%02d",
-                    lapCounter);
-            String lap = formatedLapCounter + formatedDifference + formatedCurrentTime;
-            lapView.setText(Html.fromHtml(lap));
-            lapView.setTextSize(TypedValue.COMPLEX_UNIT_SP,20);
-            layoutLaps.addView(lapView);
-            scrollView.post(() -> scrollView.fullScroll(View.FOCUS_DOWN));
+            this.addLap();
         }
+    }
+
+    private void reset() {
+        deciseconds = 0;
+        handler.post(timerRunnable);
+        buttonResetLap.setImageResource(R.drawable.lap);
+        buttonResetLap.setVisibility(View.GONE);
+        layoutLaps.removeAllViews();
+        lapCounter = 0;
+        previousLapTime = 0;
+    }
+
+    private void addLap() {
+        int difference = deciseconds - previousLapTime;
+        previousLapTime = deciseconds;
+        lapCounter++;
+        String formatedDifference = "&#160&#160&#160&#160&#160<font color=#FF0000>" + "+"
+                + formatToTime(difference)
+                + "&#160&#160&#160&#160&#160</font>";
+        String formatedCurrentTime = "<font color=#000000>" + formatToTime(deciseconds)
+                + "</font>";
+        String formatedLapCounter = String.format(Locale.getDefault(), "%02d",
+                lapCounter);
+        String lap = formatedLapCounter + formatedDifference + formatedCurrentTime;
+        lapList.add(lap);
+        this.createLapTextView(lap);
+        scrollView.post(() -> scrollView.fullScroll(View.FOCUS_DOWN));
     }
 
     private String formatToTime(int decisecs) {
@@ -126,6 +127,36 @@ public class MainActivity extends AppCompatActivity {
         int hours = (decisecs / 36000);
         return String.format(Locale.getDefault(), "%d:%02d:%02d.%d",
                 hours, minutes, seconds, decis);
+    }
+
+    private void createLapTextView(String text) {
+        TextView lapView = new TextView(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.CENTER_HORIZONTAL;
+        params.setMargins(10,10,10,10);
+        lapView.setLayoutParams(params);
+        lapView.setText(Html.fromHtml(text));
+        lapView.setTextSize(TypedValue.COMPLEX_UNIT_SP,20);
+        layoutLaps.addView(lapView);
+    }
+
+    private void loadSavedInstanceState(Bundle savedInstanceState) {
+        deciseconds = savedInstanceState.getInt("deciseconds");
+        running = savedInstanceState.getBoolean("running");
+        lapCounter = savedInstanceState.getInt("lapCounter");
+        previousLapTime = savedInstanceState.getInt("previousLapTime");
+        lapList = savedInstanceState.getStringArrayList("lapList");
+        for (String lap : lapList) {
+            this.createLapTextView(lap);
+        }
+        if (running) {
+            buttonStartStop.setImageResource(R.drawable.pause);
+            buttonResetLap.setVisibility(View.VISIBLE);
+            buttonResetLap.setImageResource(R.drawable.lap);
+        } else if (deciseconds != 0) {
+            buttonResetLap.setVisibility(View.VISIBLE);
+            buttonResetLap.setImageResource(R.drawable.reset);
+        }
     }
 
 }
